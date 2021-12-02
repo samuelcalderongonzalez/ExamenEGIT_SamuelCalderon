149a9b9faec43e19bdb7ac7cd658e1880da44e88
Samuel <a_samuel.calderon.gonzalez@iespablopicasso.es>
2021-12-02 14:19:33 +0100

    Implementadas funciones multiplicación y división

--- src/calculadora.java
+++ src/calculadora.java
@@ -9,6 +9,8 @@ public class calculadora {
 		
 		System.out.println("Suma :" + Suma(n1,n2));
 		System.out.println("Resta :" + Resta(n1,n2));
+		System.out.println("Multiplicación :" + Mul(n1,n2));
+		System.out.println("División :" + Div(n1,n2));
 		sc.close();
 	}
 	public static int Suma(int n1, int n2) {
@@ -17,6 +19,16 @@ public class calculadora {
 	}
 	public static int Resta(int n1, int n2) {
 		int resultado = n1 - n2;
+		return resultado;
+	}
+	public static int Mul(int n1, int n2) {
+		int resultado = n1 * n2;
+		return resultado;
+	}
+	public static int Div(int n1, int n2) {
+		if(n2 == 0)
+			return 0;										//Para no complicarme la vida, si divido entre 0 es 0
+		int resultado = n1 / n2;
 		return resultado;
 	}
 }
